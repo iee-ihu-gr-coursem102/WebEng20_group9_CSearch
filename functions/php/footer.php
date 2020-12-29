1cdbcb07a206477e91f64562c0ac79ea36fa6412
pmsetd <73962923+pmsetd@users.noreply.github.com>
2020-12-29 22:51:34 +0200

    Add files via upload

--- functions/php/footer.php
+++ functions/php/footer.php
@@ -9,16 +9,11 @@
     <div class="container-fluid">
 		<div class="row">
 			<div class="col-md-4 page-footer-group">
-				<p class="page-footer-header header-3">Σημαντική Πηροφορία <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-square-fill" viewBox="0 0 16 16">
-  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.93 4.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-</svg></p>
-				<p>Αυτή η διαδικτυακή εφαρμογή αντλεί δεδομένα που παρέχονται από το Songkick API</p>
+				<p class="page-footer-header header-3">Information</p>
+				<p>Αυτή η διαδικτυακή εφαρμογή αντλεί δεδομένα που παρέχονται από το <a href="https://www.songkick.com/developer">Songkick API</a></p>
 			</div>
 			<div class="col-md-4 page-footer-group">
-				<p class="page-footer-header header-3">Team Members <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-badge-cc-fill" viewBox="0 0 16 16">
-  <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm3.027 4.002c-.83 0-1.319.642-1.319 1.753v.743c0 1.107.48 1.727 1.319 1.727.69 0 1.138-.435 1.186-1.05H7.36v.114c-.057 1.147-1.028 1.938-2.342 1.938-1.613 0-2.518-1.028-2.518-2.729v-.747C2.5 6.051 3.414 5 5.018 5c1.318 0 2.29.813 2.342 2v.11H6.213c-.048-.638-.505-1.108-1.186-1.108zm6.14 0c-.831 0-1.319.642-1.319 1.753v.743c0 1.107.48 1.727 1.318 1.727.69 0 1.139-.435 1.187-1.05H13.5v.114c-.057 1.147-1.028 1.938-2.342 1.938-1.613 0-2.518-1.028-2.518-2.729v-.747c0-1.7.914-2.751 2.518-2.751 1.318 0 2.29.813 2.342 2v.11h-1.147c-.048-.638-.505-1.108-1.187-1.108z"/>
-</svg>
-				</p>
+				<p class="page-footer-header header-3">Team Members</p>
 				<ul class="page-footer-menu menu">
 					<li class="menu-item">
 						<b>Project Manager:</b> Κόκκινου Ξανθή
